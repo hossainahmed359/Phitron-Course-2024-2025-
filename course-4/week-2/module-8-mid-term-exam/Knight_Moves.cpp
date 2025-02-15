210a29534ae4b1c7b4f669754e67a7c948a0759d
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-02-16 02:28:01 +0600

    Knight Moves

--- course-4/week-2/module-8-mid-term-exam/Knight_Moves.cpp
+++ course-4/week-2/module-8-mid-term-exam/Knight_Moves.cpp
@@ -1,14 +1,15 @@
-// https://www.hackerrank.com/contests/mid-term-exam-a-introduction-to-algorithms-a-batch-06/challenges/knight-moves-2
+// https://www.hackerrank.com/contests/mid-term-exam-a-introduction-to-algorithms-a-batch-06/challenges/knight-moves-2/submissions/code/1388677837
 // Knight Moves
-
 #include <bits/stdc++.h>
 using namespace std;
 
 int n, m;
-int grid[105][105];
-bool freq[105][105];
+int ki, kj, qi, qj; /* k -> source, q -> destination */
 
-vector<pair<int, int>> move_axis = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+// int grid[105][105];
+bool freq[105][105];
+pair<int, int> parent[1005][1005];
+vector<pair<int, int>> move_axis = {{-2, -1}, {-2, 1}, {2, -1}, {2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}};
 
 bool valid_axis(int i, int j)
 {
@@ -17,79 +18,106 @@ bool valid_axis(int i, int j)
     return true;
 }
 
-int steps = 0;
-
-void DFS(int si, int sj)
+void BFS(int si, int sj)
 {
+    queue<pair<int, int>> que;
+    que.push({si, sj});
     freq[si][sj] = true;
-    steps++;
 
-    for (int i = 0; i < 4; i++)
+    while (!que.empty())
     {
-        int ci = si + move_axis[i].first;
-        int cj = sj + move_axis[i].second;
+        pair<int, int> par = que.front();
+        int par_i = par.first;
+        int par_j = par.second;
+        que.pop();
 
-        if (valid_axis(ci, cj) && !freq[ci][cj] && grid[ci][cj] == '.')
-            DFS(ci, cj);
+        for (int i = 0; i < 8; i++)
+        {
+            int child_i = par_i + move_axis[i].first;
+            int child_j = par_j + move_axis[i].second;
+
+            if (valid_axis(child_i, child_j) && !freq[child_i][child_j])
+            {
+                que.push({child_i, child_j});
+                freq[child_i][child_j] = true;
+                parent[child_i][child_j] = {par_i, par_j};
+            }
+        }
     }
 }
 
 int main()
 {
 
-    cin >> n >> m;
-    for (int i = 0; i < n; i++)
+    int t;
+    cin >> t;
+    while (t--)
     {
-        for (int j = 0; j < m; j++)
+        cin >> n >> m >> ki >> kj >> qi >> qj;
+
+        // memset(grid, 0, sizeof(grid));
+
+        for (int i = 0; i < n; i++)
         {
-            char ch;
-            cin >> ch;
-            grid[i][j] = ch;
-        }
-    }
-
-    memset(freq, false, sizeof(freq));
-    memset(freq, 0, sizeof(freq));
-
-    int min_steps = INT_MAX;
-    bool flag = false;
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            if (!freq[i][j])
+            for (int j = 0; j < m; j++)
             {
-                //
+                parent[i][j] = {-1, -1};
             }
         }
-    }
 
-    if (!flag)
-        cout << -1 << endl;
-    else
-        cout << min_steps << endl;
+        memset(freq, false, sizeof(freq));
+
+        BFS(ki, kj);
+
+        if (!freq[qi][qj])
+            cout << -1 << endl;
+        else
+        {
+            vector<pair<int, int>> path;
+            int i = qi, j = qj;
+            while (!(i == ki && j == kj))
+            {
+                path.push_back({i, j});
+
+                int temp_i = parent[i][j].first;
+                int temp_j = parent[i][j].second;
+
+                i = temp_i;
+                j = temp_j;
+            }
+
+            path.push_back({ki, kj});
+
+            reverse(path.begin(), path.end());
+
+            cout << path.size() - 1 << endl;
+        }
+    }
 
     return 0;
 }
 
 
 /* 
-6 5
-..-..
-..-..
------
-.-...
-.----
-.....
+Input -> 
+4
+8 8
+0 0
+7 7
+5 6
+0 1
+0 1
+4 4
+0 0
+0 1
+2 2
+0 0
+0 1
 
--> Output -> 3
-
-3 3
----
----
----
-
--> Output -> -1
+Output -> 
+6
+0
+3
+-1
 
 */
