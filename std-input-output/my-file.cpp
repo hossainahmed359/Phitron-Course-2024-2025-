1b57c1587144c3d71b3969e1bc9a203052fe0f2e
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-02-23 00:09:43 +0600

    Update code execution changes

---
+++ std-input-output/my-file.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+
+    int n;
+    cin >> n;
+    cout << "Printing value of n: " << n;
+
+    return 0;
+}
+
+/* 
+Open the Command Palette (Ctrl+Shift+P ).
+
+Search for and select Tasks: Run Build Task.
+
+*/
