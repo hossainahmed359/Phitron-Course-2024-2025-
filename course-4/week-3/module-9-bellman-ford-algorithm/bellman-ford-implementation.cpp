617d60dcd1b0a273ed8fd7d68a8f0de89989a6b1
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-02-23 17:24:31 +0600

    Update : bellaman ford negative weighted cycle detection

--- course-4/week-3/module-9-bellman-ford-algorithm/bellman-ford-implementation.cpp
+++ course-4/week-3/module-9-bellman-ford-algorithm/bellman-ford-implementation.cpp
@@ -15,9 +15,11 @@ public:
 int n, e;
 vector<Edge> edge_list;
 int dis[1005];
+bool cycle = false;
 
-void BELLMAN_FORD()
+void BELLMAN_FORD() /* O(V*E) */
 {
+    // MAIN CODE
     for (int i = 0; i < n - 1; i++)
     {
         for (auto ed : edge_list)
@@ -29,6 +31,17 @@ void BELLMAN_FORD()
             if (dis[a] != INT_MAX && dis[a] + c < dis[b])
                 dis[b] = dis[a] + c;
         }
+    }
+
+    // DETECT NEGATIVE WEIGHTED CYCLE
+    for (auto ed : edge_list)
+    {
+        int a = ed.a,
+            b = ed.b,
+            c = ed.c;
+
+        if (dis[a] != INT_MAX && dis[a] + c < dis[b])
+            cycle = true;
     }
 }
 
@@ -42,6 +55,7 @@ int main()
         int a, b, c;
         cin >> a >> b >> c;
         edge_list.push_back(Edge(a, b, c));
+        // edge_list.push_back(Edge(b, a, c)); /* UNDIRECTED */
     }
 
     // SET ALL DISTANCE TO MAX VALUE
@@ -56,6 +70,11 @@ int main()
     for (int i = 0; i < n; i++)
         cout << i << " -> " << dis[i] << endl;
 
+    if (cycle)
+        cout << "NEGATIVE WEIGHTED CYCLE FOUND" << endl;
+    else
+        cout << "NO NEGATIVE WEIGHTED CYCLE FOUND" << endl;
+
     return 0;
 }
 
@@ -68,12 +87,26 @@ Input:
 2 1 2
 1 3 3
 
-
 Output:
 0 -> 0
 1 -> 7
 2 -> 5
 3 -> 10
+NO NEGATIVE WEIGHTED CYCLE FOUND
+
+Input:
+4 4
+0 1 5
+1 2 3
+2 3 2
+3 1 -6
+
+Output:
+0 -> 0
+1 -> 2
+2 -> 6
+3 -> 8
+NEGATIVE WEIGHTED CYCLE FOUND
 
 
 */
