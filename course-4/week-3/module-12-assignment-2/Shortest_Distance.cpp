a5887ba36e25310ef60688396ad0ff2d61985bd6
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-03-01 19:28:43 +0600

    Update: Shortest Distance

--- course-4/week-3/module-12-assignment-2/Shortest_Distance.cpp
+++ course-4/week-3/module-12-assignment-2/Shortest_Distance.cpp
@@ -4,9 +4,68 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n, e, q;
+long long int adj_mat[1005][1005];
+
+void FLOYD_WARSHALL()
+{
+    for (int k = 1; k <= n; k++)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                long long int
+                    ik = adj_mat[i][k],
+                    kj = adj_mat[k][j],
+                    ij = adj_mat[i][j];
+
+                if (ik == LLONG_MAX || kj == LLONG_MAX)
+                    continue;
+
+                if (ik + kj < ij)
+                    adj_mat[i][j] = ik + kj;
+            }
+        }
+    }
+}
+
 int main()
 {
-    // Write your code here
+    cin >> n >> e;
+
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            adj_mat[i][j] = LLONG_MAX;
+
+    for (int i = 1; i <= n; i++)
+        adj_mat[i][i] = 0;
+
+    while (e--)
+    {
+        int a, b;
+        long long int c;
+        cin >> a >> b >> c;
+
+        if (c < adj_mat[a][b])
+            adj_mat[a][b] = c;
+    }
+
+    FLOYD_WARSHALL();
+
+    cin >> q;
+    while (q--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        long long int min_dis = adj_mat[a][b];
+
+        if (min_dis == LLONG_MAX)
+            cout << -1 << endl;
+        else
+            cout << min_dis << endl;
+    }
 
     return 0;
 }
