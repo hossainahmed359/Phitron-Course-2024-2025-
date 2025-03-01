577ee36c287bafa996cbd648f79ba2604f58677c
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-03-01 16:05:13 +0600

    Init module 12 assignment

---
+++ course-4/week-3/module-12-assignment-2/Shortest_Distance.cpp
@@ -0,0 +1,12 @@
+// https://www.hackerrank.com/contests/assignment-02-a-introduction-to-algorithms-a-batch-06/challenges/shortest-distance-2
+// Shortest Distance
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // Write your code here
+
+    return 0;
+}
---
+++ course-4/week-3/module-12-assignment-2/Water.cpp
@@ -0,0 +1,15 @@
+// https://www.hackerrank.com/contests/assignment-02-a-introduction-to-algorithms-a-batch-06/challenges/water-4-1
+// Water
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
