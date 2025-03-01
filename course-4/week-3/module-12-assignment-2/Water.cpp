e140d547a6bd53514bcdf1d727988732a97b3031
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-03-01 22:28:16 +0600

    Update: Water

--- course-4/week-3/module-12-assignment-2/Water.cpp
+++ course-4/week-3/module-12-assignment-2/Water.cpp
@@ -7,9 +7,47 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+int main()
+{
 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<long long int> height(n);
 
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+        for (int i = 0; i < n; i++)
+        {
+            long long int h;
+            cin >> h;
+            height[i] = h;
+        }
+
+        int max_index = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (height[i] > height[max_index])
+                max_index = i;
+        }
+        int second_max_index;
+        if (max_index == 0)
+            second_max_index = 1;
+        else
+            second_max_index = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i != max_index && height[i] > height[second_max_index])
+                second_max_index = i;
+        }
+
+        if (second_max_index < max_index)
+            cout << second_max_index << " " << max_index << endl;
+        else
+            cout << max_index << " " << second_max_index << endl;
+    }
+
     return 0;
 }
