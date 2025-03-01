0e50fe28ddc72a20d210ec1d9beadf26162a5e00
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-03-01 20:04:30 +0600

    add solution of : Cycle of Edges:

--- course-4/week-3/module-12-assignment-2/Cycle_of_Edges.cpp
+++ course-4/week-3/module-12-assignment-2/Cycle_of_Edges.cpp
@@ -4,9 +4,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n, e;
+int cycle_count = 0;
+vector<int> par(100005, -1);
+vector<int> group_size(100005, 1);
+
+int find_lead(int node)
+{
+    if (par[node] == -1)
+        return node;
+
+    int lead = find_lead(par[node]);
+    par[node] = lead;
+
+    return lead;
+}
+
+void dsu_union(int node_a, int node_b)
+{
+    int lead_a = find_lead(node_a);
+    int lead_b = find_lead(node_b);
+
+    if (group_size[lead_a] >= group_size[lead_b])
+    {
+        par[lead_b] = lead_a;
+        group_size[lead_a] += group_size[lead_b];
+    }
+    else
+    {
+        par[lead_a] = lead_b;
+        group_size[lead_b] += lead_a;
+    }
+}
+
 int main()
 {
-    // Write your code here
+    cin >> n >> e;
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        int lead_a = find_lead(a),
+            lead_b = find_lead(b);
+
+        if (lead_a == lead_b)
+            cycle_count++;
+        else
+            dsu_union(a, b);
+    }
+
+    cout << cycle_count << endl;
 
     return 0;
 }
