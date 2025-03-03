8b9c920be129407226520dff5cb70b2a08c41d42
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-03-03 23:56:36 +0600

    Start Introduction to dynamic programming

---
+++ course-4/week-4/module-13-practice-problem/roads-not-only-in-berland.cpp
@@ -0,0 +1,9 @@
+// https://codeforces.com/problemset/problem/25/D
+// D. Roads not only in Berland
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    return 0;
+}
---
+++ course-4/week-4/module-14-dynamic-programming/dynamic-programming.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "Introduction to dynamic programming";
+
+    return 0;
+}
