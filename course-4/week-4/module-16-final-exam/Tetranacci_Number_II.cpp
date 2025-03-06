b8e21453fbb1561af7adb2ab426cfa65c9273fed
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-03-07 03:05:25 +0600

    Add solution to : Tetranacci Number II

--- course-4/week-4/module-16-final-exam/Tetranacci_Number_II.cpp
+++ course-4/week-4/module-16-final-exam/Tetranacci_Number_II.cpp
@@ -8,8 +8,26 @@
 #include <algorithm>
 using namespace std;
 
+int n;
+vector<long long int> series(105, -1);
+
 int main()
 {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    series[0] = 0, series[1] = 1, series[2] = 1, series[3] = 2;
+    cin >> n;
+
+    if (n <= 3)
+    {
+        cout << series[n] << endl;
+        return 0;
+    }
+
+    for (int i = 4; i <= n; i++)
+    {
+        series[i] = series[i - 1] + series[i - 2] + series[i - 3] + series[i - 4];
+    }
+
+    cout << series[n] << endl;
+
     return 0;
 }
