85dc7efff813af1e91448d5925b46a2fb261cccc
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-03-07 02:59:41 +0600

    Add Tetranacci Number I solution

--- course-4/week-4/module-16-final-exam/Tetranacci_Number_I.cpp
+++ course-4/week-4/module-16-final-exam/Tetranacci_Number_I.cpp
@@ -8,8 +8,34 @@
 #include <algorithm>
 using namespace std;
 
+/*
+T0 = 0, T1 = 1, T2 = 1,T3 = 2
+For  , Tn = Tn-1 + Tn-2 + Tn-3 + Tn-4
+*/
+
+int n;
+vector<long long int> series(105, -1);
+
+int tetranacci(int n)
+{
+    if (n <= 3)
+        return series[n];
+
+    if (series[n] != -1)
+        return series[n];
+
+    series[n] = tetranacci(n - 1) + tetranacci(n - 2) + tetranacci(n - 3) + tetranacci(n - 4);
+
+    return series[n];
+}
+
 int main()
 {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    series[0] = 0, series[1] = 1, series[2] = 1, series[3] = 2;
+    cin >> n;
+
+    long long int val = tetranacci(n);
+    cout << val << endl;
+
     return 0;
 }
