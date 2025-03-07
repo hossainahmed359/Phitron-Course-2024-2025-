2cb76ab8f0bf5d59a53035bc878597c4d3577f20
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-03-07 17:07:00 +0600

    add initial solution of make it

--- course-4/week-4/module-16-final-exam/Make_It.cpp
+++ course-4/week-4/module-16-final-exam/Make_It.cpp
@@ -4,9 +4,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<bool> freq(100005, false);
+
+void make_it(int i, int n)
+{
+    cout << "i : " << i << ", n: " << n << endl;
+
+    if (i > n || freq[n])
+    {
+        return;
+    }
+    freq[i] = true;
+
+    make_it(i + 3, n);
+    make_it(i * 2, n);
+}
+
 int main()
 {
-    // Write your code here
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        make_it(1, n);
+
+        if (freq[n])
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
 
     return 0;
 }
