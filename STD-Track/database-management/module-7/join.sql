2f7afe2b72e8916c7391ce4aa70a9f686107249f
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-07-24 00:44:32 +0600

    add basic examples of sql joins

---
+++ STD-Track/database-management/module-7/join.sql
@@ -0,0 +1,48 @@
+USE dummydb;
+
+
+-- INNER, LEFT, RIGHT, CROSS, SELF
+
+
+SELECT * FROM employees;
+SELECT * FROM departments;
+
+-- Without Using Join Syntax | Not Efficient
+SELECT CONCAT(employees.first_name, ' ', employees.last_name) AS 'full name', departments.department_name  
+From employees, departments 
+    WHERE employees.department_id = departments.department_id;
+
+-- With Join Syntax | ON
+SELECT CONCAT(employees.first_name, ' ', employees.last_name) AS 'full name', departments.department_name  
+FROM employees 
+    JOIN departments ON employees.department_id = departments.department_id;
+
+
+-- With Join Syntax | USING | Needs to have exact column name
+SELECT CONCAT(employees.first_name, ' ', employees.last_name) AS 'full name', departments.department_name  
+FROM employees 
+    JOIN departments USING(department_id);
+
+-- Inner Join
+SELECT CONCAT(employees.first_name, ' ', employees.last_name) AS 'full name', departments.department_name  
+FROM employees 
+    JOIN departments ON employees.department_id = departments.department_id;
+
+SELECT CONCAT(employees.first_name, ' ', employees.last_name) AS 'full name', departments.department_name  
+FROM employees 
+    INNER JOIN departments ON employees.department_id = departments.department_id; -- JOIN -> INNER JOIN
+
+-- Left Join
+SELECT CONCAT(employees.first_name, ' ', employees.last_name) AS 'full name', departments.department_name  
+FROM employees 
+    LEFT JOIN departments ON employees.department_id = departments.department_id;
+
+-- Right Join
+SELECT CONCAT(employees.first_name, ' ', employees.last_name) AS 'full name', departments.department_name  
+FROM employees 
+    RIGHT JOIN departments ON employees.department_id = departments.department_id;
+
+-- Cross Join
+SELECT CONCAT(employees.first_name, ' ', employees.last_name) AS 'full name', departments.department_name  
+FROM employees 
+    CROSS JOIN departments ON employees.department_id = departments.department_id;
