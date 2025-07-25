a6d827e00e2084b14f2c3d3d65df4e1f2e7e21f6
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-07-26 03:09:32 +0600

    add module 8

--- STD-Track/database-management/module-8/query.sql
+++ STD-Track/database-management/module-8/query.sql
@@ -32,8 +32,56 @@ FROM
 
 --
 --
--- QUESTION 3: PENDING
+-- QUESTION 3: 
+-- Create a table of Employees which has the following fields
+-- Employee_Id
+-- First Name
+-- Last Name
+-- Date of Birth
+-- Department Id
+-- Salary
+-- Create a table Projects with the following fields:	
+-- Project ID
+-- Project Name
+-- Start Date
+-- End Date
+-- Budget
+-- Create a table Employee_Projects with the following fields:
+-- Employee ID	
+-- Project ID
+-- Ensure that each employee can work on multiple projects and a project can have multiple employees.
 -- ANS: 
+CREATE TABLE
+    employees (
+        employee_id INT,
+        first_name VARCHAR(20),
+        last_name VARCHAR(20),
+        date_of_birth DATE,
+        department_id INT,
+        salary DECIMAL(8, 0),
+        CONSTRAINT employee_pk_rule PRIMARY KEY (employee_id),
+        CONSTRAINT employee_date_of_birth_rule CHECK (date_of_birth IS NOT NULL),
+        CONSTRAINT employee_department_id_rule CHECK (department_id IS NOT NULL)
+    );
+
+CREATE TABLE
+    projects (
+        project_id INT,
+        project_name VARCHAR(20),
+        start_date DATE,
+        end_date DATE,
+        budget DECIMAL(8, 0),
+        CONSTRAINT project_pk_rule PRIMARY KEY (project_id),
+        CONSTRAINT project_name_rule CHECK (project_name IS NOT NULL)
+    );
+
+CREATE TABLE
+    employee_projects (
+        employee_id INT,
+        project_id INT,
+        CONSTRAINT employee_projects_pk_rule PRIMARY KEY (employee_id, project_id)
+    );
+
 --
 --
 -- QUESTION 4: Using the dummydb, write an SQL query to get the third-highest salary in the employees table.
@@ -152,15 +200,48 @@ WHERE
 
 --
 --
--- QUESTION 8: 
+-- QUESTION 8: Write a query to display the names of employees who earn a salary less than the employee "Steven King".
 -- ANS: 
+USE dummydb;
+
+SELECT
+    CONCAT (first_name, ' ', last_name) AS 'Full Name'
+FROM
+    employees
+WHERE
+    salary < (
+        SELECT
+            salary
+        FROM
+            employees
+        WHERE
+            CONCAT (first_name, ' ', last_name) = 'Steven King'
+    );
+
 --
 --
--- QUESTION 9:
+-- QUESTION 9: Write a query to find the department names and the names of the managers for each department.
 -- ANS: 
+USE dummydb;
+
+SELECT
+    d.department_name,
+    CONCAT (e.first_name, ' ', e.last_name) AS 'manager_name'
+FROM
+    departments AS d
+    LEFT JOIN employees AS e ON e.employee_id = d.manager_id;
+
 --
 --
--- QUESTION 10:
+-- QUESTION 10: Write a query to display the names of all cities where departments are located.
 -- ANS: 
+USE dummydb;
+
+SELECT
+    l.city
+FROM
+    departments AS d
+    LEFT JOIN locations AS l ON d.location_id = l.location_id;
+
 --
 --
