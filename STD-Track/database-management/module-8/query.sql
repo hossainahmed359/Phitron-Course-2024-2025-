ba98088f6167696b66da6fe2c6c1db243bb71748
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-07-25 20:53:07 +0600

    Update: module-8 question 7

--- STD-Track/database-management/module-8/query.sql
+++ STD-Track/database-management/module-8/query.sql
@@ -14,6 +14,8 @@ USE dummydb;
 -- QUESTION 2: What is a Self Join? Provide an example using the employees table to list employees who share the same manager ID, showing only their first names and manager IDs.
 -- ANS: 
 -- Self Join is a join operation where a table is joined with itself and returns joined table based on the ON clause.
+USE dummydb;
+
 SELECT
     e1.first_name,
     e1.manager_id
@@ -70,23 +72,84 @@ FROM
     ) AS e
     RIGHT JOIN departments AS d ON e.department_id = d.department_id;
 
--- SELECT
---     COUNT(department_id) AS number_of_employees,
---     department_id
--- FROM
---     employees
--- WHERE
---     department_id IS NOT NULL
--- GROUP BY
---     department_id;
 --
 --
--- QUESTION 6: 
+-- QUESTION 6: Illustrate INNER JOIN, LEFT JOIN, RIGHT JOIN, and CROSS JOIN with examples using the employees and departments tables.
 -- ANS: 
+USE dummydb;
+
+-- INNER JOIN
+SELECT
+    e.employee_id,
+    e.first_name,
+    d.department_name
+FROM
+    employees AS e
+    JOIN departments AS d ON e.department_id = d.department_id;
+
+-- LEFT JOIN
+SELECT
+    e.employee_id,
+    e.first_name,
+    d.department_name
+FROM
+    employees AS e
+    JOIN departments AS d ON e.department_id = d.department_id;
+
+-- RIGHT JOIN
+SELECT
+    d.department_name,
+    e.number_of_employees
+FROM
+    (
+        SELECT
+            COUNT(department_id) AS number_of_employees,
+            department_id
+        FROM
+            employees
+        WHERE
+            department_id IS NOT NULL
+        GROUP BY
+            department_id
+    ) AS e
+    RIGHT JOIN departments AS d ON e.department_id = d.department_id;
+
+-- CROSS JOIN
+SELECT
+    e.employee_id,
+    e.first_name,
+    d.department_name
+FROM
+    employees AS e
+    CROSS JOIN departments AS d;
+
 --
 --
--- QUESTION 7: 
+-- QUESTION 7: What is a Common Table Expression (CTE)? Write an example query using CTE to show the names of employees whose salary is higher than the average salary
 -- ANS: 
+-- CTE OR Common Table Expression a temporary named result set that is defined with in the execution scope
+USE dummydb;
+
+WITH
+    avg_t AS (
+        SELECT
+            AVG(salary) as avg_salary
+        FROM
+            employees
+    )
+SELECT
+    CONCAT (first_name, ' ', last_name) AS 'Full Name',
+    salary
+FROM
+    employees
+WHERE
+    salary > (
+        SELECT
+            avg_salary
+        FROM
+            avg_t
+    );
+
 --
 --
 -- QUESTION 8: 
